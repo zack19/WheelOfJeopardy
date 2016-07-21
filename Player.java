1964f7ed09a8bea8f45ac2cfe5bae17242d19bf4
Zachary Bubb <zack19@vt.edu>
2016-07-20 22:21:28 -0400

    Add Values and Methods

--- Player.java
+++ Player.java
@@ -2,5 +2,44 @@ package teamAgile;
 
 public class Player
 {
+    String name;
+    private int points;
+    private int freePlayTokens;
 
+    public Player(String name)
+    {
+        this.name = name;
+        points = 0;
+        freePlayTokens = 0;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Update Player Points
+     * @param pointChange
+     */
+    public void updatePoints(int pointChange)
+    {
+
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Update Player Tokens
+     * @param tokenChange
+     */
+    public void updateTokens(int tokenChange)
+    {
+
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Ask if player wants to use token
+     * @return player choice
+     */
+    public Boolean useToken()
+    {
+        return true;
+    }
 }
