7dfb93a55665395f3849f437e52ddd6b23faaa02
Zachary Bubb <zack19@vt.edu>
2016-07-20 22:21:58 -0400

    Update Question.java

--- Question.java
+++ Question.java
@@ -2,5 +2,12 @@ package teamAgile;
 
 public class Question
 {
+    private int pointValue;
+    private Boolean isAnswered;
 
+    public Question(int points)
+    {
+        pointValue = points;
+        isAnswered = false;
+    }
 }
