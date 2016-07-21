dbee9fb538afdb8d3eff320bd003de9e5d4d20b4
Zachary Bubb <zack19@vt.edu>
2016-07-20 22:20:42 -0400

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -2,6 +2,11 @@ package teamAgile;
 
 public class PlayGame
 {
+    private Player[] playerList;
+    private int round;
+    private String[] wheel;
+    private int remainingSpins;
+
     public PlayGame()
     {
         System.out.println("Game Started");
