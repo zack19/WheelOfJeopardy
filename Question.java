2abad8d08738e4339bdb7d31728a5274d68f97d0
Zachary Bubb <zack19@vt.edu>
2016-07-27 22:29:53 -0400

    Add category

--- Question.java
+++ Question.java
@@ -4,10 +4,12 @@ public class Question
 {
     private int pointValue;
     private Boolean isAnswered;
+    private String category;
 
-    public Question(int points)
+    public Question(int points, String category)
     {
         pointValue = points;
+        this.category = category;
         isAnswered = false;
     }
 }
