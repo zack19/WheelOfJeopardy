b4c6ba4050ff9e333d1aef9c70aecebcad8d38dc
Zachary Bubb <zack19@vt.edu>
2016-08-03 20:25:53 -0400

    Add Skeleton

--- Scoreboard.java
+++ Scoreboard.java
@@ -3,4 +3,49 @@ package teamAgile;
 public class Scoreboard
 {
 
+    public Scoreboard()
+    {
+        //Blank Constructor
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Loop through player list and print each player and points
+     * @param playerList
+     */
+    public void displayPoints(Player[] playerList)
+    {
+        return;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Dsiplay Round
+     * @param round
+     */
+    public void displayRound(int round)
+    {
+        //add some formating
+        System.out.println(round);
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Print players names
+     * @param playerList
+     */
+    public void displayPLayers(Player[] playerList)
+    {
+        return;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * print out the winning player
+     * @param playerList
+     */
+    public void declareWinner(Player[] playerList)
+    {
+        return;
+    }
 }
